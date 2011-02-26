1db7df1bd380f4e5661dcc8de0e39e848460e597
Christian Anton <mail@christiananton.de>
2011-02-27 00:06:43 +0100

    added config file parser to cronjob.sh and implemented a test omitting fritzboxuploader when egw2fbox.pl did not finish with exitcode 0

--- bin/cronjob.sh
+++ bin/cronjob.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-BASEDIR=/Users/kellinge/egw2fbox
+while getopts  "c:" flag
+do
+  case "$flag" in
+	c)
+		CONFIGFILE=$OPTARG
+		;;
+  esac
+done
+
+if [ -z $CONFIGFILE ]; then
+	echo "you must supply a config file using the -c argument"
+	exit 253
+
+elif [ ! -f "$CONFIGFILE" ]; then
+	echo "could not open config file"
+	exit 254
+fi
+
+BASEDIR=$(grep "CRON_BASEDIR" $CONFIGFILE | cut -d"=" -f2|sed "s/ //g"|sed "s/\/\+$//")
+
+if [ ! -d "$BASEDIR" ]; then
+	echo "basedir $BASEDIR not found or is not a directory"
+	exit 252
+fi
+
 BINDIR=$BASEDIR/bin
 CONFDIR=$BASEDIR/etc
 DATADIR=$BASEDIR/data
@@ -11,6 +35,10 @@ cd $BASEDIR
 
 # call the magic perl script
 $BINDIR/egw2fbox.pl -c $CONFDIR/egw2fbox.conf
+if [ $? -ne 0 ]; then
+	echo "egroupware exporter did not finish correctly"
+	exit 251
+fi
 
 # hash
 NEWHASH=$(cat $DATADIR/phonebook.xml | grep -v mod_time | md5sum | cut -d" " -f1)
