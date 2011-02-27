9d008046f974f97caf67255251acffba464d17fe
Christian Anton <mail@christiananton.de>
2011-02-27 12:55:55 +0100

    added -v for VERBOSE mode in cronjob.sh also passing -v to the egw2fbox perl script

--- bin/cronjob.sh
+++ bin/cronjob.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+### FUNCS
+function verbose {
+	if [ -n "$VERBOSE" ]
+	then
+		echo "$0: $1"
+	fi
+}
+
 ### INIT
 # read command line option: cronjob.sh -c config.file 
-while getopts "c:" flag
+while getopts "c:v" flag
 do
   case "$flag" in
 	c)
 		CONFIGFILE=$OPTARG
 		;;
+	v)
+		VERBOSE=1
+		;;
   esac
 done
 
 # read config file
+verbose "reading config file..."
 if [ -z $CONFIGFILE ]
 then
 	echo "you must supply a config file using the -c argument"
@@ -41,17 +53,32 @@ umask 077
 cd $BASEDIR
 
 # call the magic perl script
-$BINDIR/egw2fbox.pl -c $CONFIGFILE
+if [ -n "$VERBOSE" ]
+then
+	verbose "about to start the worker script in verbose mode"
+	EGW2FBOX_ARGS="-c $CONFIGFILE -v"
+else
+	verbose "about to start the worker script"
+	EGW2FBOX_ARGS="-c $CONFIGFILE"
+fi
+
+$BINDIR/egw2fbox.pl $EGW2FBOX_ARGS
 if [ $? -ne 0 ]; then
 	echo "egroupware exporter did not finish correctly"
 	exit 251
+else
+	verbose "worker script ended successfully"
 fi
 
 # hash
 NEWHASH=$(cat $DATADIR/phonebook.xml | grep -v mod_time | md5sum | cut -d" " -f1)
 OLDHASH=$(cat $DATADIR/phonebook.hash 2>/dev/null)
 
-if [ "_$OLDHASH" != "_$NEWHASH" ]; then
+if [ "_$OLDHASH" = "_$NEWHASH" ]
+then
+	verbose "hashes of last and new output xml do not differ, not updating fritzbox"
+else
+	verbose "now uploading new XML file to FritzBox"
 	export FRITZUPLOADERCFG=$CONFIGFILE
 	$BINDIR/fritzuploader.pl
 
@@ -61,5 +88,8 @@ if [ "_$OLDHASH" != "_$NEWHASH" ]; then
 		# make this wrapper script recognize that another upload is
 		# needed
 		echo $NEWHASH >$DATADIR/phonebook.hash
+		verbose "upload succeeded"
+	else
+		verbose "upload could not be finished correctly"
 	fi
 fi
