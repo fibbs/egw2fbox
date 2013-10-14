5c2ff9f4d2bf7d2fc32924ae89e24cbdab407f12
Kai Ellinger <coding@blicke.de>
2013-10-14 14:09:34 +0200

    added mark down readme

--- bin/create_docs.sh
+++ bin/create_docs.sh
@@ -1,7 +1,11 @@
 pod2readme egw2fbox.pl ../docs/README
 pod2html --infile=egw2fbox.pl --outfile=../docs/html/egw2fbox.html --title "egw2fbox documentation"
 pod2man egw2fbox.pl ../docs/man/egw2fbox.1
+pod2markdown egw2fbox.pl README.md.tmp
+cat README.md.tmp | perl -e '$doPrint = 1; while(defined ( my $line = <STDIN>) ) { if ($line =~/^# INSTALLATION/) { $doPrint = 1; } if ($line =~/^# AUTHORS/) { $doPrint = 1; } if ($line =~/^# HISTORY/) { $doPrint = 0; } if ($line =~/^# API/) { $doPrint = 0; } if($doPrint) { print $line; } } ' >../README.md
 perl egw2fbox.pl --changelog >../docs/CHANGELOG
 find ../docs -name "*.bak" -exec rm {} \;
 find ../docs -ls
+ls -la ../README*
 rm *.tmp
+
